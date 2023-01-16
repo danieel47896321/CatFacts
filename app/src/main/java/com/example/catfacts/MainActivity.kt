fb1f4ab22cd0e27986737afd24cf622305622b1f
Daniel <danieel47896321@gmail.com>
2023-01-16 12:23:16 +0200

    created fact view. created main adapter. finish main xml.

--- app/src/main/java/com/example/catfacts/MainActivity.kt
+++ app/src/main/java/com/example/catfacts/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.catfacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import androidx.recyclerview.widget.RecyclerView
+import com.example.catfacts.adapter.MainAdapter
+import com.example.catfacts.api.CatJson
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var buttonGetFact: Button
+    private lateinit var progressBar: ProgressBar
+    private var factList = ArrayList<CatJson>()
+    private var mainAdapter: MainAdapter = MainAdapter(factList)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+    private fun init() {
+        recyclerView = findViewById(R.id.recyclerView)
+        buttonGetFact = findViewById(R.id.buttonGetFact)
+        progressBar = findViewById(R.id.progressBar)
+        recyclerView.adapter = mainAdapter
+        buttonGetFact()
+    }
+    private fun buttonGetFact() {
+        buttonGetFact.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/catfacts/adapter/MainAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.catfacts.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.catfacts.R
+import com.example.catfacts.api.CatJson
+
+class MainAdapter(private var factList: ArrayList<CatJson>): RecyclerView.Adapter<MainAdapter.ViewHolder>()  {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.fact_view,parent,false)
+        return ViewHolder(v)
+    }
+    override fun onBindViewHolder(holder: MainAdapter.ViewHolder, position: Int) {
+        holder.textViewFact.text = factList[position].text
+    }
+    override fun getItemCount(): Int {
+        return factList.size
+    }
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        var textViewFact: TextView
+        init {
+            textViewFact = view.findViewById(R.id.textViewFact)
+        }
+    }
+}
